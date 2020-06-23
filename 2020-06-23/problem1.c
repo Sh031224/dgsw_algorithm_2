d2230905cd767f09d45f7a82d15b75e73d8f7440
Sh031224 <1cktmdgh2@gmail.com>
2020-06-23 16:51:17 +0900

    [ADD] 2020-06-23

---
+++ 2020-06-23/problem1.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+void main() {
+	int input;
+	int result = 0;
+
+	scanf("%d", &input);
+
+	for (int i = 1; i < input; i++) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			result += i;
+		}
+	}
+
+	printf("%d", result);
+}
---
+++ 2020-06-23/problem2.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+void main() {
+	unsigned long long int input;
+	unsigned long long int max = 0;
+
+	scanf("%lld", &input);
+
+	for (unsigned long long int i = 2; i * i <= input; i++) {
+
+		while (input % i == 0) {
+			if (max < i) {
+				max = i;
+			}
+			input /= i;
+		}
+	}
+
+	if (input != 1) {
+		printf("%lld", input);
+	}
+
+
+}
